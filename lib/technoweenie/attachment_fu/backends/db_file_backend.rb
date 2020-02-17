f197f6fa98e3121c13dc3e322eeac3a17311bca2
Alan Rogers <alanjrogers@me.com>
2020-02-17 15:33:38 +1100

    More modern fixes.

--- lib/technoweenie/attachment_fu/backends/db_file_backend.rb
+++ lib/technoweenie/attachment_fu/backends/db_file_backend.rb
@@ -12,25 +12,29 @@ module Technoweenie # :nodoc:
         def create_temp_file
           write_to_temp_file current_data
         end
-        
+
         # Gets the current data from the database
         def current_data
           db_file.data
         end
-        
+
         protected
           # Destroys the file.  Called in the after_destroy callback
           def destroy_file
             db_file.destroy if db_file
           end
-          
+
           # Saves the data to the DbFile model
           def save_to_storage
             if save_attachment?
               (db_file || build_db_file).data = temp_data
               db_file.save!
               self.db_file_id = db_file.id
-              self.class.where(:id => id).update_all(:db_file_id => db_file.id)
+              if self.class.respond_to?(:where)
+                self.class.where(:id => id).update_all(:db_file_id => db_file.id)
+              else
+                self.class.update_all({ :db_file_id => db_file.id }, { :id => id })
+              end
             end
             true
           end
